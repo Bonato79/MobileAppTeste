beabf8bc8f152d0812dbe5e5acf6bead0632456a
aries <jaijunior11@gmail.com>
2023-06-20 23:30:56 -0300

    Funcionalidade funcional, porém aparentemente com erro na tela

--- app/src/main/java/desenvolvimento/app/mobileappteste/CameraActivity.java
+++ app/src/main/java/desenvolvimento/app/mobileappteste/CameraActivity.java
@@ -1,188 +1,58 @@
 package desenvolvimento.app.mobileappteste;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+public class CameraActivity extends Activity {
 
-import java.util.Arrays;
-
-public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback {
-
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1001;
-
-    private CameraManager cameraManager;
-    private CameraDevice cameraDevice;
-    private CameraCaptureSession cameraCaptureSession;
-    private CaptureRequest.Builder previewRequestBuilder;
-    private SurfaceView firstHalfSurfaceView;
-    private SurfaceView secondHalfSurfaceView;
-    private SurfaceHolder holder;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
 
-        firstHalfSurfaceView = findViewById(R.id.first_half_surfaceview);
-        secondHalfSurfaceView = findViewById(R.id.second_half_surfaceview);
+        // Configurar o WebView
+        webView = new WebView(this);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Ativar suporte a JavaScript, se necessário
+        webSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36");
 
-        firstHalfSurfaceView.getHolder().addCallback(this);
+        // Definir a atividade como tela cheia
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA},
-                    CAMERA_PERMISSION_REQUEST_CODE);
-        } else {
-            openCamera();
-        }
+        // Definir a atividade como sobrepondo outros aplicativos
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+        // Definir a atividade como sobrepondo notificações
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+
+        // Carregar uma URL específica
+        webView.loadUrl("https://sketchfab.com/models/faf4babdcda04c3aa95192736503802e/embed?autostart=1&cardboard=1&internal=1&tracking=0&ui_infos=0&ui_snapshots=1&ui_stop=0&ui_theatre=1&ui_watermark=0");
+
+        // Definir o WebView como conteúdo da atividade
+        setContentView(webView);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        closeCamera();
-    }
+    protected void onDestroy() {
+        super.onDestroy();
 
-    private void openCamera() {
-        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-            String cameraId = cameraManager.getCameraIdList()[0];
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            cameraManager.openCamera(cameraId, cameraStateCallback, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
+        // Liberar recursos do WebView
+        if (webView != null) {
+            webView.destroy();
         }
-    }
-
-    private void closeCamera() {
-        if (cameraCaptureSession != null) {
-            cameraCaptureSession.close();
-            cameraCaptureSession = null;
-        }
-        if (cameraDevice != null) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-    }
-
-    private void startCameraPreview() {
-        try {
-            SurfaceHolder firstHalfSurfaceHolder = firstHalfSurfaceView.getHolder();
-            Surface firstHalfSurface = firstHalfSurfaceHolder.getSurface();
-            SurfaceHolder secondHalfSurfaceHolder = secondHalfSurfaceView.getHolder();
-            Surface secondHalfSurface = secondHalfSurfaceHolder.getSurface();
-
-            previewRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            previewRequestBuilder.addTarget(firstHalfSurface);
-            previewRequestBuilder.addTarget(secondHalfSurface);
-
-            cameraDevice.createCaptureSession(Arrays.asList(firstHalfSurface, secondHalfSurface),
-                    new CameraCaptureSession.StateCallback() {
-                        @Override
-                        public void onConfigured(@NonNull CameraCaptureSession session) {
-                            if (cameraDevice == null) {
-                                return;
-                            }
-                            previewRequestBuilder.set(CaptureRequest.CONTROL_CAPTURE_INTENT, CaptureRequest.CONTROL_CAPTURE_INTENT_PREVIEW);
-
-                            cameraCaptureSession = session;
-                            try {
-                                previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
-                                CaptureRequest previewRequest = previewRequestBuilder.build();
-                                cameraCaptureSession.setRepeatingRequest(previewRequest, null, null);
-                            } catch (CameraAccessException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        @Override
-                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                            Log.e("CameraActivity", "Failed to configure camera capture session.");
-                        }
-                    }, null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-            cameraDevice = camera;
-            startCameraPreview();
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice camera) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice camera, int error) {
-            cameraDevice.close();
-            cameraDevice = null;
-        }
-    };
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        openCamera();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        // No implementation needed
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        closeCamera();
-    }
-
-    private int getJpegOrientation(Context context, int cameraId) throws CameraAccessException {
-        CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(Integer.toString(cameraId));
-
-        int sensorOrientation = cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
-        int deviceOrientation = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
-
-        int rotation = 0;
-        switch (deviceOrientation) {
-            case Surface.ROTATION_0:
-                rotation = 0;
-                break;
-            case Surface.ROTATION_90:
-                rotation = 90;
-                break;
-            case Surface.ROTATION_180:
-                rotation = 180;
-                break;
-            case Surface.ROTATION_270:
-                rotation = 270;
-                break;
-        }
-
-        int jpegOrientation = (sensorOrientation - rotation + 360) % 360;
-        return jpegOrientation;
     }
 }
+
+
