c26df2cf663f1d3b5eda77cf93dc81270e0b58fd
aries <jaijunior11@gmail.com>
2023-06-21 21:19:59 -0300

    Mudança da CameraView para tentar abrir navegador em tela separada

--- app/src/main/java/desenvolvimento/app/mobileappteste/CameraActivity.java
+++ app/src/main/java/desenvolvimento/app/mobileappteste/CameraActivity.java
@@ -1,165 +1,44 @@
 package desenvolvimento.app.mobileappteste;
-
-import android.app.Activity;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.WindowManager;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
+import android.webkit.ValueCallback;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class CameraActivity extends Activity implements SensorEventListener {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private WebView webView;
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-
-    private static final int THRESHOLD = 5; // Limiar de movimento para ativar a simulação de teclas
-    private boolean isMovingForward, isMovingBackward, isMovingLeft, isMovingRight;
+public class CameraActivity extends AppCompatActivity {
+    private WebView webView1;
+    private WebView webView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
 
-        // Configurar a atividade como tela cheia
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        webView1 = findViewById(R.id.webview1);
+        webView2 = findViewById(R.id.webview2);
 
-        // Configurar a atividade como sobrepondo outros aplicativos
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
-                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
+        // Carrega uma URL na primeira WebView
+        webView1.loadUrl("https://sketchfab.com/3d-models/totem-oral-b-faf4babdcda04c3aa95192736503802e");
 
-        // Criar o WebView
-        webView = new WebView(this);
-        webView.setWebChromeClient(new WebChromeClient());
+        // Configura um WebViewClient para a primeira WebView
+        webView1.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
 
-        // Configurar as configurações do WebView
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true); // Ativar suporte a JavaScript, se necessário
-        webSettings.setDomStorageEnabled(true); // Ativar armazenamento DOM, se necessário
-
-        // Carregar uma URL específica
-        webView.loadUrl("https://sketchfab.com/models/faf4babdcda04c3aa95192736503802e/embed?autostart=1&cardboard=1&internal=1&tracking=0&ui_infos=0&ui_snapshots=1&ui_stop=0&ui_theatre=1&ui_watermark=0");
-
-        // Configurar o WebView como conteúdo da atividade
-        setContentView(webView);
-
-        // Inicializar o sensor de acelerômetro
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        webView.onResume();
-        webView.resumeTimers();
-
-        // Registrar o listener do sensor de acelerômetro
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        webView.onPause();
-        webView.pauseTimers();
-
-        // Remover o listener do sensor de acelerômetro
-        sensorManager.unregisterListener(this);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Remover o WebView do layout
-        if (webView != null) {
-            webView.setVisibility(View.GONE);
-            webView.removeAllViews();
-            webView.destroy();
-        }
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
-
-        // Detectar movimento para frente e trás
-        if (Math.abs(x) > THRESHOLD) {
-            if (x > 0) {
-                if (!isMovingForward) {
-                    simulateKeyPress(KeyEvent.KEYCODE_DPAD_UP, true);
-                    isMovingForward = true;
-                }
-            } else {
-                if (!isMovingBackward) {
-                    simulateKeyPress(KeyEvent.KEYCODE_DPAD_DOWN, true);
-                    isMovingBackward = true;
-                }
+                // Obtém o título e a URL da primeira WebView
+                webView1.evaluateJavascript(
+                        "(function() { return {title: document.title, url: document.URL}; })();",
+                        new ValueCallback<String>() {
+                            @Override
+                            public void onReceiveValue(String value) {
+                                // Insere as informações na segunda WebView
+                                String js = "javascript:document.open();document.write('<html><body>' + " + value + ".title + '<br><br>' + " + value + ".url + '</body></html>');document.close();";
+                                webView2.loadUrl(js);
+                            }
+                        });
             }
-        } else {
-            if (isMovingForward) {
-                simulateKeyPress(KeyEvent.KEYCODE_DPAD_UP, false);
-                isMovingForward = false;
-            }
-            if (isMovingBackward) {
-                simulateKeyPress(KeyEvent.KEYCODE_DPAD_DOWN, false);
-                isMovingBackward = false;
-            }
-        }
-
-        // Detectar movimento para esquerda e direita
-        if (Math.abs(y) > THRESHOLD) {
-            if (y > 0) {
-                if (!isMovingRight) {
-                    simulateKeyPress(KeyEvent.KEYCODE_DPAD_RIGHT, true);
-                    isMovingRight = true;
-                }
-            } else {
-                if (!isMovingLeft) {
-                    simulateKeyPress(KeyEvent.KEYCODE_DPAD_LEFT, true);
-                    isMovingLeft = true;
-                }
-            }
-        } else {
-            if (isMovingRight) {
-                simulateKeyPress(KeyEvent.KEYCODE_DPAD_RIGHT, false);
-                isMovingRight = false;
-            }
-            if (isMovingLeft) {
-                simulateKeyPress(KeyEvent.KEYCODE_DPAD_LEFT, false);
-                isMovingLeft = false;
-            }
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Não é necessário implementar esse método para este caso
-    }
-
-    private void simulateKeyPress(int keyCode, boolean isKeyDown) {
-        int action = isKeyDown ? KeyEvent.ACTION_DOWN : KeyEvent.ACTION_UP;
-        long now = System.currentTimeMillis();
-        KeyEvent keyEvent = new KeyEvent(now, now, action, keyCode, 0);
-        dispatchKeyEvent(keyEvent);
-    }
-
-    public boolean dispatchKeyEvent(KeyEvent keyEvent) {
-        // Simular o pressionamento de tecla no WebView
-        webView.dispatchKeyEvent(keyEvent);
-        return false;
+        });
     }
 }
